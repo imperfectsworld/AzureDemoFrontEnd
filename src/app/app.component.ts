4162411bba94ac51efe7f31b223fd907211edeeb
imperfectsworld <imperfectsworld@gmail.com>
2024-10-07 14:28:54 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BackendService } from './services/backend.service';
+import { Post } from './models/post';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AzureFrontEndDemo';
+  formPost:Post = {} as Post;
+  allPosts:Post[] = [];
+  constructor (private backService:BackendService) {}
+
+  getAll(){
+    this.backService.getAllPosts().subscribe(response => {
+      console.log(response);
+      this.allPosts = response;
+    });
+
+  }
+
+  ngOnInit(){
+    this.getAll();
+  }
+
+addPost(){
+  //temp fix until login
+  this.formPost.googleId = "1";
+  this.backService.addPost(this.formPost).subscribe(response => {
+    console.log(response);
+    this.getAll();
+  })
+}
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiDomain: "https://localhost:7106/" // replace this with your back API's localhost url 
+  };
